515a07f4d98039960ff245a672030062ec4bbbfb
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-07-07 23:48:32 +0100

    Enhance responsive design for Row component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ function Dashboard() {
   return (
     <>
       <Row type="horizontal">
-        <Heading as="h2">Dashboard</Heading>
+        <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
       </Row>
 
